418c121532adb3bcef80e9894b960945266172db
criscrispy <jlcaddy4@gmail.com>
2020-12-16 18:53:56 +0200

    Updates

---
+++ precache-manifest.db5aa0ab87ca8607f6e704c327d0e815.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5159c34df08764b46878",
+    "url": "/lights-out-game/static/css/main.7ed4ed40.chunk.css"
+  },
+  {
+    "revision": "5159c34df08764b46878",
+    "url": "/lights-out-game/static/js/main.5159c34d.chunk.js"
+  },
+  {
+    "revision": "b43a7eab4c27c21e54e5",
+    "url": "/lights-out-game/static/js/1.b43a7eab.chunk.js"
+  },
+  {
+    "revision": "c21eb99d7eac6d163be2",
+    "url": "/lights-out-game/static/js/runtime~main.c21eb99d.js"
+  },
+  {
+    "revision": "45151ea7fe025bb406cf776892589f79",
+    "url": "/lights-out-game/index.html"
+  }
+];
